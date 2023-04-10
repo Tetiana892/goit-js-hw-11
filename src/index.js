cbfdcab56ee8b34eea796fdbbf32578a62fb5afd
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-04-10 14:14:32 +0300

    add templates and renderphoto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
 import { PixabayAPI } from './js/PixabayAPI';
-import { renderGallery } from './js/render-gallery';
+
+const pixabayApi = new PixabayAPI();
+
+async function onRenderPage(page) {
+  try {
+    const respons = await pixabayApi.fetchPhotosByQuery(page);
+    console.log(respons.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+onRenderPage();
--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -4,25 +4,11 @@ export class PixabayAPI {
   #API_KEY = '35202570-a98fb6affeab795733fe4a227';
   #query = '';
 
-  getPopularPhotos(page) {
-    return axios.get(`${this.#BASE_URL}/search/photos`, {
-      params: {
-        q,
-        page: '1',
-        per_page: '40',
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: ' true',
-        key: this.#API_KEY,
-      },
-    });
-  }
-
   fetchPhotosByQuery(page) {
-    return axios.get(`${this.#BASE_URL}/search/photos`, {
+    return axios.get(`${this.#BASE_URL}`, {
       params: {
-        q: this.#query,
-        page: '1',
+        q: 'random',
+        page,
         per_page: '40',
         image_type: 'photo',
         orientation: 'horizontal',
