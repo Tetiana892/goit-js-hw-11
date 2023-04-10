11a4ee88bba4def7868b55821e71bcd8b902eac4
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-04-10 18:47:32 +0300

    add scss

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import './css/styles.css';
+import './sass/index.scss';
 import { PixabayAPI } from './js/PixabayAPI';
+import createGalleryCard from './templates/gallery-cards.hbs';
 
+const galleryEl = document.querySelector('.gallery');
 const pixabayApi = new PixabayAPI();
 
 async function onRenderPage(page) {
   try {
     const respons = await pixabayApi.fetchPhotosByQuery(page);
-    console.log(respons.data);
+    galleryEl.innerHTML = createGalleryCard(respons.data.hits);
+    console.log(respons.data.hits);
   } catch (error) {
     console.log(error);
   }
