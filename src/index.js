e3598b9337bd62a3e936ad5a6ef4be71d6bf63f8
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-04-12 19:29:51 +0300

    add function submit and  btnMore

--- src/index.js
+++ src/index.js
@@ -3,25 +3,38 @@ import { PixabayAPI } from './js/PixabayAPI';
 import createGalleryCard from './templates/gallery-cards.hbs';
 
 const galleryEl = document.querySelector('.gallery');
-
-// const options = {
-//   root: null,
-//   rootMargin: '100px',
-//   threshold: 1.0,
-// };
+const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.btn-load-more');
 
 const pixabayApi = new PixabayAPI();
-// const pagination = new IntersectionObserver(onRenderPage, options);
-// const page = pagination.get();
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onRenderPage(page) {
   try {
     const respons = await pixabayApi.fetchPhotosByQuery(page);
     galleryEl.innerHTML = createGalleryCard(respons.data.hits);
-    console.log(respons.data.total);
-    // pagination.reset(respons.data.total);
+    // console.log(respons.data.total);
   } catch (error) {
     console.log(error);
   }
 }
 onRenderPage();
+
+function onSearch(e) {
+  e.preventDefault();
+
+  pixabayApi.query = e.currentTarget.elements.searchQuery.value.trim();
+  pixabayApi.resetPage();
+  // galleryEl.innerHTML = '';
+  pixabayApi.fetchPhotosByQuery().then(appendMarkup);
+}
+
+function onLoadMore() {
+  pixabayApi.fetchPhotosByQuery().then(appendMarkup);
+}
+
+function appendMarkup(cards) {
+  galleryEl.insertAdjacentElement('beforeend', createGalleryCard(cards));
+}
